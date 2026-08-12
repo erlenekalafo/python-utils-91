aacb539e036b5418c081e8c4778ed9ac76ae9d1f
Erlen Kalafo <erlenekalafo@inbox.lv>
2026-08-12 14:14:14 +0300

    Optimize logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,33 +1,18 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-# Configure logging settings
-logging.basicConfig(level=logging.INFO, 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def setup_logger(log_file='app.log', max_bytes=5*1024*1024, backup_count=2):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
-class Logger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-    def info(self, message):
-        self.logger.info(message)
+    return logger
 
-    def warning(self, message):
-        self.logger.warning(message)
-
-    def error(self, message):
-        self.logger.error(message)
-
-    def debug(self, message):
-        self.logger.debug(message)
-
-    def exception(self, message):
-        self.logger.exception(message)
-
-# Example usage:
+# Example of using the logger setup
 if __name__ == '__main__':
-    log = Logger(__name__)
-    log.info('This is an info message')
-    log.warning('This is a warning message')
-    log.error('This is an error message')
-    log.debug('This is a debug message')
-    log.exception('This is an exception message')
+    my_logger = setup_logger()
+    my_logger.info('Logger is set up and ready to use.')
